407a08dcdbf3955174051b95e4534c42192c9ab9
Nate <natoe33@gmail.com>
2022-10-11 16:42:59 -0400

    working on navbar

---
+++ src/components/NavItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <li class="sidebar-item">
+        <router-link to="{{link}}">
+            {{listing}}
+        </router-link>
+    </li>
+</template>
+<script>
+export default {
+  name: "NavItem",
+  props: {
+    listing: {
+      type: String,
+      default: null
+    },
+    link: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,20 +1,26 @@
 <template>
-<!-- https://blog.colorffy.com/posts/how-to-create-an-animated-sidebar-with-vue-3 -->
   <div>
-    <div class="sidebar">
+    <div :class="['sidebar', isSidebarActive  ? '' : 'sidebar-hidden']">
       <div class="sidebar-content">
         <div class="sidebar-header">
           <p>Components menu</p>
-          <button class="btn btn-sm btn-link">
-            X
+          <button class="btn btn-sm btn-link" @click="sidebarToggle()">
+            <i class="material-icons-outlined">close</i>
           </button>
         </div>
         <div class="sidebar-body">
           <ul>
+            <NavItem v-for="listing in data" :listing="listing.listing" :link="listing.link" />
             <li class="sidebar-item">
               <router-link to="/">
                 <i class="material-icons-outlined">code</i>
                 Sidebar
+              </router-link>
+            </li>
+            <li class="sidebar-item disabled">
+              <router-link to="/" :disabled="isDisabled">
+                <i class="material-icons-outlined">code</i>
+                Buttons
               </router-link>
             </li>
           </ul>
@@ -28,3 +34,41 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import NavItem from "./NavItem.vue";
+const response = await fetch('http://192.168.4.97:8090/api/listings', {
+  mode: 'cors'
+})
+const data = await response.json()
+
+</script>
+
+<script>
+export default {
+  name: "Sidebar",
+  components: {
+    NavItem
+  },
+  props: {
+    isSidebarActive: {
+      type: Boolean,
+      default: null
+    },
+    sidebarToggle: {
+      type: Function,
+      default: null
+    }
+  },
+  data() {
+    return {
+      isDisabled: true
+    }
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
