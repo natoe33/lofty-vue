f3e6e2620df94c5261aacd730265152f8f58982f
Nate Flateau <natoe33@gmail.com>
2022-10-11 09:13:11 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import Sidebar from "./components/Sidebar.vue";
+import { SidebarMenu } from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
+</script>
+<script>
+export default {
+  components: {
+    SidebarMenu,
+    Sidebar
+  },
+  data() {
+    return {
+      menu: [
+        {
+          header: "Main Navigation",
+          hiddenOnCollapse: true,
+        },
+        {
+          href: "/",
+          title: "Dashboard",
+          icon: "fa fa-user",
+        },
+        {
+          href: "/about",
+          title: "About",
+          icon: "fa fa-user",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+<Sidebar />
+  <!-- <SidebarMenu class="sidebar" :menu="menu" /> -->
+  <!-- <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+  <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+  </b-sidebar> -->
+
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+<style lang="scss">
+@import './assets/main.scss'
+</style>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.sidebar {
+  clear: both;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- https://blog.colorffy.com/posts/how-to-create-an-animated-sidebar-with-vue-3 -->
+  <div>
+    <div class="sidebar">
+      <div class="sidebar-content">
+        <div class="sidebar-header">
+          <p>Components menu</p>
+          <button class="btn btn-sm btn-link">
+            X
+          </button>
+        </div>
+        <div class="sidebar-body">
+          <ul>
+            <li class="sidebar-item">
+              <router-link to="/">
+                <i class="material-icons-outlined">code</i>
+                Sidebar
+              </router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="sidebar-footer">
+          <a href="https://blog.colorffy.com" target="_blank" rel="noopener">
+            By: blog.colorffy.com
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
